cebb305b43acbe397da005b2cfcd6fe24c84a4d7
jideabdqudus <jideabdqudus@gmail.com>
2020-09-25 16:18:27 +0100

    Added Commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const connectDB = require("./config/db")
+
+connectDB()
+
 app.get("/", (req, res) => res.json({ msg: "Welcome to the API..." }));
 
 app.use("/api/users", require("./routes/users"));
