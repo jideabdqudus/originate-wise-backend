7b4110621f34e9627bf4a519ad7f346d240a5f33
jideabdqudus <jideabdqudus@gmail.com>
2020-09-25 16:49:20 +0100

    Added Commit

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,30 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
+const Users = require("../models/Users");
 //@route POST api/users
 //@desc  Register a user
 //@access  Public
-router.post("/", (req, res)=>{
-    res.send("Register a user")
-})
+router.post(
+  "/",
+  [
+    check("firstname", "First Name is required").not().isEmpty(),
+    check("lastname", "Last Name is required").not().isEmpty(),
+    check("email", "Please include a valide email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
 
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("passed");
+  }
+);
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
 
-connectDB()
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.json({ msg: "Welcome to the API..." }));
 
